c111f5734119f7fdb43ed8fbf5eddd846af993a4
dddotcom <dddotcom@gmail.com>
2015-04-16 16:21:30 -0400

    added saving Request object to database in process_request; note: this requires a migration for a Request model

--- middlewares.py
+++ middlewares.py
@@ -1,23 +1,33 @@
 import traceback
 import sys
 import time
+from demo_webapp.models import Request
 
 class Repair(object):
 
     def process_request(self, request):
-        # Open request log file
-        f = open('requests.log', 'a')
+        timestamp = str(int(time.time()))
+        full_url = str(request.get_host()) + str(request.get_full_path())
+        host = str(request.get_host())
+        url_path = str(request.get_full_path())
+        is_good = True
 
         # Log GET requests
         if request.method == 'GET':
-            f.write(str(time.strftime("%Y-%m-%d %H:%M:%S")) + ";" + str(request.META["REMOTE_ADDR"]) + ";" + str(request.get_host()) + ";" + str(request.get_full_path()) + ";" + str(request.method) + ";" + str(request.GET) + "\n")
-            f.close()
-
+            param_map = request.GET
         # Log POST requests
         if request.method == 'POST':
-            f.write(str(time.strftime("%Y-%m-%d %H:%M:%S")) + ";" + str(request.META["REMOTE_ADDR"]) + ";" + str(request.get_host()) + ";" + str(request.get_full_path()) + ";" + str(request.method) + ";" + str(request.POST) + "\n")
-            f.close()
+            param_map = request.POST
 
+        #save to the database
+        if len(param_map) != 0 and "delete_selected" not in str(param_map):
+            r = Request(timestamp=timestamp, full_url=full_url, host=host, url_path=url_path, is_good=is_good, param_map=param_map)
+            r.save()
+        
+        # AND/OR write to file 
+        f = open('requests.log', 'a')
+        f.write(timestamp + ";" + full_url + ";" + host + ";" + url_path + ";" + str(is_good) + ";" +  str(param_map) + "\n")
+        f.close()
         return
 
     def process_exception(self,request, exception):
