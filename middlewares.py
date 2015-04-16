17b48fe320714c963a0b2bded8180565978f46f2
minniek <minniek@bu.edu>
2015-04-16 11:33:17 -0400

    added process_request function to middlewares.py

--- middlewares.py
+++ middlewares.py
@@ -1,7 +1,24 @@
 import traceback
 import sys
+import time
 
 class Repair(object):
+
+    def process_request(self, request):
+        # Open request log file
+        f = open('requests.log', 'a')
+
+        # Log GET requests
+        if request.method == 'GET':
+            f.write(str(time.strftime("%Y-%m-%d %H:%M:%S")) + ";" + str(request.META["REMOTE_ADDR"]) + ";" + str(request.get_host()) + ";" + str(request.get_full_path()) + ";" + str(request.method) + ";" + str(request.GET) + "\n")
+            f.close()
+
+        # Log POST requests
+        if request.method == 'POST':
+            f.write(str(time.strftime("%Y-%m-%d %H:%M:%S")) + ";" + str(request.META["REMOTE_ADDR"]) + ";" + str(request.get_host()) + ";" + str(request.get_full_path()) + ";" + str(request.method) + ";" + str(request.POST) + "\n")
+            f.close()
+
+        return
 
     def process_exception(self,request, exception):
         #print traceback.print_stack()
@@ -12,4 +29,3 @@ class Repair(object):
             print traceback.extract_tb(tb)
         finally:
             del tb
-       
